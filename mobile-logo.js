9dfe7b0811d5a8539857d474ee9af226c8853809
RewitzerTS <richard.rewitzerzwhe@gmail.com>
2026-08-13 17:19:10 +0200

    Remove unused mobile logo script

--- mobile-logo.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  if (document.querySelector('.mobile-top-logo')) return;
-  const logo = document.createElement('img');
-  logo.className = 'mobile-top-logo';
-  logo.src = 'assets/top-sports-logo.svg';
-  logo.alt = 'Top Sports Fitness';
-  document.body.append(logo);
-})();
